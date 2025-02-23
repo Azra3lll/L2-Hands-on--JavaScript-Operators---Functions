14fe99f0b5d1bffe1bb836a6aa38ab4b9b236a19
Azra3lll <rjdelosreyes39@gmail.com>
2025-02-24 01:10:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let sum = num1 + num2;
+
+alert("The sum is: " + sum);
